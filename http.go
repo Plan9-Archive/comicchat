ef74c1ea4502f2e6142dd7a71c65b3b28b6bda68
mischief <mischief@offblast.org>
2014-02-14 03:55:53 -0800

    connect client to irc, some html controls

--- http.go
+++ http.go
@@ -2,14 +2,105 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
-	"encoding/json"
+	"fmt"
 	"github.com/bitly/go-notify"
+	"github.com/kballard/goirc/irc"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
 type NewUrl struct {
 	Url string
+}
+
+type WebClientMessage struct {
+	Type    string
+	Message string
+}
+
+// a websocket connection to irc
+type WebClient struct {
+	nick string
+
+	config  irc.Config
+	irc     irc.SafeConn
+	ircquit chan bool
+
+	ws *websocket.Conn
+}
+
+func NewWebClient(ws *websocket.Conn) *WebClient {
+	nick := fmt.Sprintf("comicchat%5d", rand.Intn(99999))
+	wc := &WebClient{
+		config: irc.Config{
+			Host:     "chat.freenode.net",
+			Nick:     nick,
+			User:     "comicchat",
+			RealName: "Comic Chat",
+		},
+		ws: ws,
+	}
+
+	wc.config.Init = func(hr irc.HandlerRegistry) {
+		hr.AddHandler(irc.CONNECTED, wc.loggedin)
+	}
+
+	return wc
+}
+
+func (w *WebClient) loggedin(conn *irc.Conn, line irc.Line) {
+	conn.Join([]string{"#comicchat"}, nil)
+}
+
+func (w *WebClient) reader() {
+	var err error
+	w.irc, err = irc.Connect(w.config)
+	if err != nil {
+		w.ws.Close()
+	}
+	for {
+		var m WebClientMessage
+		err = websocket.JSON.Receive(w.ws, &m)
+		if err != nil {
+			log.Printf("%s read: %s", w.ws.RemoteAddr(), err)
+			break
+		}
+
+		if w.irc == nil {
+			continue
+		}
+
+		switch m.Type {
+		case "nick":
+			w.irc.Nick(m.Message)
+		case "privmsg":
+			w.irc.Privmsg("#comicchat", m.Message)
+		case "action":
+			w.irc.Action("#comicchat", m.Message)
+		case "connect":
+		}
+	}
+}
+
+func (w *WebClient) writer() {
+	imgurlchan := make(chan interface{})
+	notify.Start("newimage", imgurlchan)
+	defer notify.Stop("newimage", imgurlchan)
+
+loop:
+	for {
+		select {
+		case url := <-imgurlchan:
+			var m WebClientMessage
+			m.Type = "newimage"
+			m.Message = url.(string)
+			if err := websocket.JSON.Send(w.ws, &m); err != nil {
+				log.Printf("%s closed: %s", w.ws.RemoteAddr(), err)
+				break loop
+			}
+		}
+	}
 }
 
 func dohttp() {
@@ -22,21 +113,7 @@ func dohttp() {
 }
 
 func newimagehandler(ws *websocket.Conn) {
-	imgurlchan := make(chan interface{})
-	notify.Start("newimage", imgurlchan)
-	defer notify.Stop("newimage", imgurlchan)
-
-loop:
-	for {
-		select {
-		case url := <-imgurlchan:
-			var u NewUrl
-			u.Url = url.(string)
-			je := json.NewEncoder(ws)
-			if err := je.Encode(&u); err != nil {
-				log.Printf("%s closed: %s", ws.RemoteAddr(), err)
-				break loop
-			}
-		}
-	}
+	wc := NewWebClient(ws)
+	go wc.writer()
+	wc.reader()
 }
