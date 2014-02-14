c8a9e8500a9b0df08cd4ccd8f00c412f262c5f2b
mischief <mischief@offblast.org>
2014-02-14 00:14:33 -0800

    websockets!

--- http.go
+++ http.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"code.google.com/p/go.net/websocket"
+	"encoding/json"
 	"github.com/bitly/go-notify"
 	"log"
 	"net/http"
 )
 
+type NewUrl struct {
+	Url string
+}
+
 func dohttp() {
-	http.HandleFunc("/new", newimagehandler)
+	http.Handle("/new", websocket.Handler(newimagehandler))
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("static/"))))
 	http.Handle("/comic/", http.StripPrefix("/comic/", http.FileServer(http.Dir("comic/"))))
-	if err := http.ListenAndServe(":4005", nil); err != nil {
+	if err := http.ListenAndServe(":8899", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
 
-func newimagehandler(c http.ResponseWriter, req *http.Request) {
+func newimagehandler(ws *websocket.Conn) {
 	imgurlchan := make(chan interface{})
 	notify.Start("newimage", imgurlchan)
 	defer notify.Stop("newimage", imgurlchan)
 
-	fmt.Fprintf(c, "%s", <-imgurlchan)
+loop:
+	for {
+		select {
+		case url := <-imgurlchan:
+			var u NewUrl
+			u.Url = url.(string)
+			je := json.NewEncoder(ws)
+			if err := je.Encode(&u); err != nil {
+				log.Printf("%s closed: %s", ws.RemoteAddr(), err)
+				break loop
+			}
+		}
+	}
 }
